48f21199b955ca856ac68749688c971e830767b3
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-08-08 14:29:48 +0900

    :white_check_mark: 5-4

--- part02/todo/src/main.rs
+++ part02/todo/src/main.rs
@@ -1,9 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, http::header, post, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
 use thiserror::Error;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -29,6 +40,32 @@ enum MyError {
 }
 
 impl ResponseError for MyError {}
+
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+
+    return Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish());
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id = ?", &[&params.id])?;
+
+    return Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish());
+}
 
 #[get("/")]
 async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
@@ -71,10 +108,16 @@ async fn main() -> Result<(), actix_web::Error> {
     const PORT: u16 = 8080;
     println!("Listening on http://127.0.0.1:{}", PORT);
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
-        .bind(format!("{}:{}", ADDRESS, PORT))?
-        .run()
-        .await?;
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+    })
+    .bind(format!("{}:{}", ADDRESS, PORT))?
+    .run()
+    .await?;
 
     return Ok(());
 }
